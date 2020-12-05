c6bcf95cffb6126b2a90d99a46451b1540470e4d
Ananta Kr. Roy <kr.innovate@gmail.com>
2020-12-05 23:24:30 +0530

    day 5 AoC complete

---
+++ demo.js
@@ -0,0 +1,9 @@
+var getCode = (function() {
+    var apiCode = '0]Eal(eh&2';    // A code we do not want outsiders to be able to modify...
+    
+    return function() {
+      return apiCode;
+    };
+  })();
+  
+  getCode();    // Returns the apiCode
